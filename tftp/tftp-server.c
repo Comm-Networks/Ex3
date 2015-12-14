8c5f704bfe30125bf1f1fe9f595ec756698510da
Tomer Ben Kereth <bk.tomer@gmail.com>
2015-12-14 20:34:01 +0200

    receive is done.
    *recieved packet and  made a return packet.
    *also sent the packet back.
    *need to treat errors and edge cases.

--- tftp/tftp-server.c
+++ tftp/tftp-server.c
@@ -1,31 +1,69 @@
 #include "tftp_protocol.h"
 
+
+data_packet data_pkt;error_packet err_pkt;
+ack_packet ack_pkt;rw_packet rw_pkt;
+
+
+
+void fillErorMessage(error_packet * err_pkt,u_short err_code,char* msg){
+	err_pkt->err_code=err_code;
+	err_pkt->opcode=ntohs(ERROR);
+	err_pkt->err_msg=msg;
+}
+
+/*
+ * fill bytes buffer with the proper packet.
+ */
+void fillBuffer(char* buf, u_short opcode){
+	int size;
+	switch(opcode){
+	case(ACK):
+			memcpy(buf,&ack_pkt,sizeof(ack_packet));
+			size=sizeof(ack_packet);
+	break;
+	case(DATA):
+			memcpy(buf,&data_pkt,sizeof(data_packet));
+			size=sizeof(data_packet);
+	break;
+	case(ERROR):
+			memcpy(buf,&err_pkt,sizeof(error_packet));
+			size=sizeof(error_packet);
+	break;
+	default:
+		break;
+	}
+	memset(buf+sizeof(ack_packet),0,sizeof(buf)-size); //setting the rest of the buffer to 0.
+}
+
 int main(int argc,char** argv){
 
 
 	struct addrinfo  hints;
 	struct addrinfo * my_addr , *rp, client_addr;
 
-	socklen_t slen =sizeof(client_addr);
+	socklen_t slen;
 	const char hostname[];
 	char* port = DEFAULT_PORT;
 	unsigned int size;
 	int fd,sock;
-	int max_fd;
-	int i;
+	int ack_blk_num=0;
+	int data_blk_num=1;
 	int ret_val=0;
-	int player_turn =0; // player 0 or 1
 	int client_connected=0;
+	int offset=0;
 	char file_name[] = "new_file";
-	tftp_message recv_data,sent_data;
 	short rw_mode=0; //write-1, read-0
 	char buffer[sizeof(rw_packet)];
-
+	u_short opcode;
 	struct timeval time;
+	struct stat st_buf;
+	short final_data_block=0;
 
 
 	// Main loop.
 	while (1) {
+		//no client is connected - open a new socket and bind.
 		if (!client_connected){
 			// Obtain address(es) matching host/port
 			memset(&hints,0,sizeof(struct addrinfo));
@@ -67,32 +105,136 @@ int main(int argc,char** argv){
 			freeaddrinfo(my_addr);
 			client_connected=1;
 		}
-		ret_val =recvfrom(sock,buffer,sizeof(buffer),&client_addr,&slen);
+		slen=sizeof(client_addr);
+		/*Buffer's size is the len of the longest packet type.
+		  If the recieved packet's size is smaller => we recieve the bytes we need. */
+		ret_val =recvfrom(sock,buffer,sizeof(buffer),0,&client_addr,&slen);
 		if (ret_val<0){
 			printf("Recieving failed.Discconeting client: %s.\n",strerror(errno));
-			close(sock);
 			if (fd>0){
 				close(fd);
 			}
 			client_connected=0;
 			continue;
 		}
-		short opcode;
-		memcpy(&opcode,buffer,sizeof(short));
+
+		memcpy(&opcode,buffer,sizeof(u_short));
+		opcode = htons(opcode);
+		//check recieved packet type and prepare the output message.
 		switch (opcode){
 		case (WRQ):
-				recv_data.opcode=WRQ;
-				memcpy(&(recv_data.packet.rw_pck),&(buffer[2]),sizeof(rw_packet));
+				memcpy(&(rw_pkt),&(buffer[2]),sizeof(rw_packet));
 				rw_mode=1;//write mode
-				fd = open(recv_data.packet.rw_pck.file_name,O_RDWR | O_TRUNC | O_CREAT,S_IRWXU | S_IRWXG | S_IRWXO);
-				if (fd<0){
-					printf("Failed opening the new file.%s.\n",strerror(errno));
+
+				if ((ret_val=stat(rw_pkt.file_name,&st_buf))<0){
+					if (errno!=ENOENT){
+						printf("Error while calling stat.%s\n",strerror(errno));
+						opcode=(u_short) ERROR;
+						fillErorMessage(&err_pkt,OTHER,strerror(errno));
+					}
 				}
-				sent_data.opcode=ACK;
-				sent_data.packet.ack_pkt.block_num=1;
-				sent_data.packet.ack_pkt.opcode=ACK;
+				//file already exist
+				if (ret_val>-1){
+					opcode=(u_short)ERROR;
+					err_pkt.opcode=ntohs(opcode);
+					err_pkt.err_code=FILE_EXIST;
+					memset(err_pkt.err_msg,0,MAX_DATA_SIZE);//no need for a message here, code is enough.
+				}
+				//file does not exist. we can open a new file.
+				else {
+					fd = open(rw_pkt.file_name,O_RDONLY | O_TRUNC | O_CREAT,S_IRWXU | S_IRWXG | S_IRWXO);
+					if (fd<0){
+						printf("Failed opening the new file.%s.\n",strerror(errno));
+						opcode=(u_short) ERROR;
+						fillErorMessage(&err_pkt,OTHER,strerror(errno));
+					}
+					else{
+						opcode=(u_short)ACK;
+						ack_pkt.block_num=ack_blk_num++;
+						ack_pkt.opcode=ntohs(opcode);
+					}
+				}
 		break;
+		case (RRQ):
+				memcpy(&(rw_pkt),&(buffer[2]),sizeof(rw_packet));
+				rw_mode=0;//read mode
+				if ((ret_val=stat(rw_pkt.file_name,&st_buf))<0){
+					if (errno==ENOENT){
+						opcode=(u_short)ERROR;
+						err_pkt.err_code=FILE_NOT_FOUND;
+						err_pkt.opcode=ntohs(opcode);
+						memset(err_pkt.err_msg,0,MAX_DATA_SIZE);//no need for a message here, code is enough.
+					}
+					else{
+						printf("Error while calling stat.%s\n",strerror(errno));
+						close(sock);
+						break;
+					}
+				}
+				fd = open(rw_pkt.file_name,O_RDWR,S_IRWXU | S_IRWXG | S_IRWXO);
+				if (fd<0){
+
+					printf("Failed opening file.%s.\n",strerror(errno));
+				}
+				//set the data msg.
+				opcode=(u_short)DATA;
+				data_pkt.block_num=data_blk_num++;
+				data_pkt.opcode=ntohs(opcode);
+				ret_val=read(fd,data_pkt.data,MAX_DATA_SIZE);
+				if (ret_val<0){
+					printf("Error reading from file.%s\n",strerror(errno));
+					close(fd);
+					fillErorMessage(&err_pkt,OTHER,strerror(errno));
+				}
+				//checks if final data block recieved.
+				final_data_block = ret_val<MAX_DATA_SIZE ? 1 :0;
+		break;
+		case (DATA):
+				memcpy(&(data_pkt),&(buffer[2]),sizeof(data_packet));
+				ret_val=write(fd,data_pkt.data,MAX_DATA_SIZE);
+				if (ret_val<0){
+					printf("Error writing to file.%s\n",strerror(errno));
+					close(fd);
+					opcode=(u_short) ERROR;
+					fillErorMessage(&err_pkt,OTHER,strerror(errno));
+				}
+				//prepraring the ack reply msg.
+				opcode= (u_short) ACK;
+				ack_pkt.block_num=ack_blk_num++;
+				ack_pkt.opcode=ntohs(opcode);
+				if (ret_val<MAX_DATA_SIZE){
+					final_data_block=1;
+				}
+		break;
+		case (ACK):
+				memcpy(&(ack_pkt),&(buffer[2]),sizeof(ack_packet));
+				opcode=(u_short)DATA;
+				data_pkt.block_num=data_blk_num++;
+				data_pkt.opcode=ntohs(opcode);
+				ret_val=read(fd,data_pkt.data,MAX_DATA_SIZE);
+				//in case of failure - send error message to the client and close the file.
+				if (ret_val<0){
+					printf("Error reading from file.%s\n",strerror(errno));
+					close(fd);
+					fillErorMessage(&err_pkt,OTHER,strerror(errno));
+				}
+				else {
+					//checks if final data block recieved.
+					final_data_block = ret_val<MAX_DATA_SIZE ? 1 :0;
+				}
+		break;
+		default:
+			break;
+		}
+		fillBuffer(buffer,opcode);
+		ret_val=sendto(sock,buffer,sizeof(buffer),0,&client_addr,sizeof(client_addr));
+		if (ret_val<0){
+			//TODO: how to deal with fail of send. should I try to send error again?
 		}
 
+		//TODO  if error message, after sending close client.
 	}
+	close(sock);
+	return (ret_val==-1)? EXIT_FAILURE : EXIT_SUCCESS;
 }
+
--- tftp/tftp_protocol.h
+++ tftp/tftp_protocol.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #define MAX_CLIENTS 6
 #define DEFAULT_PORT "6900"
@@ -18,22 +19,25 @@
 #define DATA 3
 #define ACK 4
 #define ERROR 5
+#define FILE_EXIST 6
+#define FILE_NOT_FOUND 1
+#define OTHER 0
 
 #pragma pack(push, 1)
 typedef struct {
 	u_short opcode;
 	u_short block_num;
-	u_char data[MAX_DATA_SIZE];
+	char data[MAX_DATA_SIZE];
 }data_packet;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 typedef struct {
 	u_short opcode;
-	u_char file_name[FILENAME_MAX];
-	u_char zero_byte=0;
-	u_char mode[5];
-	u_char zero_byte_2=0;
+	char file_name[FILENAME_MAX];
+	char zero_byte=0;
+	char mode[5];
+	char zero_byte_2=0;
 }rw_packet;
 #pragma pack(pop)
 
@@ -47,24 +51,13 @@ typedef struct {
 #pragma pack(push, 1)
 typedef struct {
 	u_short opcode;
-	u_char file_name[FILENAME_MAX];
-	u_char err_msg[MAX_DATA_SIZE];
-	u_char zero_byte=0;
+	u_short err_code;
+	char err_msg[MAX_DATA_SIZE];
+	char zero_byte=0;
 }error_packet;
 #pragma pack(pop)
 
-#pragma pack(push,1)
-typedef union {
-	data_packet data_pkt;
-	ack_packet ack_pkt;
-	error_packet err_pck;
-	rw_packet rw_pck;
-}packet_t;
-typedef struct {
-	u_short opcode; /* tftp opcode*/
-	packet_t packet;
-}tftp_message;
-#pragma pack(pop)
+
 
 
 
